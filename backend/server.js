857f635f9a3ad8482584bcafae8f5fea0f78fcd5
DzeBaKuEso <dzeblebaku@gmail.com>
2025-07-01 03:46:02 -1200

    setup and development of backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// backend/server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
